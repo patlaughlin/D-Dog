98a17d868ebb06602151cb1f20838006897642b6
Patrick Laughlin <patrick@laughl.info>
2015-10-20 10:32:49 -0400

    - add key catching event - begin putting event handling in its own component

--- containers/App.js
+++ containers/App.js
@@ -9,10 +9,39 @@ import ToggleButton from './../components/ToggleButton';
 const { SHOW_EDITOR, SHOW_DOCUMENT } = VisibilityFilters;
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleView = this.toggleView.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.toggleView);
+  }
+
+  toggleView(e) {
+    /** Listen for CMD+E **/
+    const { dispatch, visibilityFilter } = this.props;
+    let matchesCommandE = e.keyCode === 69 && e.metaKey;
+
+    console.log(visibilityFilter);
+    if (matchesCommandE) {
+      if (visibilityFilter === SHOW_DOCUMENT) {
+        dispatch(setVisibilityFilter(SHOW_EDITOR))
+      } else if (visibilityFilter === SHOW_EDITOR) {
+        dispatch(setVisibilityFilter(SHOW_DOCUMENT))
+      }
+    }
+
+  }
+
   render() {
     const { dispatch, content, visibilityFilter } = this.props;
     return (
       <div>
+        <HotKeys
+          onKeyDown={nextFilter =>
+            dispatch(setVisibilityFilter(nextFilter))
+          }/>
         <ToggleButton
           filter={visibilityFilter}
           onFilterChange={nextFilter =>
