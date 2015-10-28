6612e5618a044af2c10c68ad4b02c18f2a71ee69
Patrick Laughlin <patrick@laughl.info>
2015-10-28 15:39:09 -0400

    - port

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use('/', express.static(static_path, {
   maxage: 31557600
 }));
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
